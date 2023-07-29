3f46b1f956b7f798ad420fa9c55f97db238b5357
Eduardo <eduardo_cardoso516@outlook.com>
2023-07-29 13:17:56 -0300

    Finalizado a parte grafica

--- Analisador/src/Grafico/analisadorPanel.java
+++ Analisador/src/Grafico/analisadorPanel.java
@@ -1,5 +1,7 @@
 package Grafico;
 
+import Base.Lexico;
+
 /**
  *
  * @author eduardo
@@ -11,6 +13,7 @@ public class analisadorPanel extends javax.swing.JFrame {
      */
     public analisadorPanel() {
         initComponents();
+        jTextPane1.setEditable(false);
     }
 
     /**
@@ -26,8 +29,9 @@ public class analisadorPanel extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
         jButton1 = new javax.swing.JButton();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTextPane1 = new javax.swing.JTextPane();
+        jLabel4 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -44,9 +48,9 @@ public class analisadorPanel extends javax.swing.JFrame {
             }
         });
 
-        jLabel2.setText("jLabel2");
+        jScrollPane2.setViewportView(jTextPane1);
 
-        jLabel3.setText("jLabel3");
+        jLabel4.setText("Analise");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -59,12 +63,14 @@ public class analisadorPanel extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel1)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel2)
-                        .addGap(53, 53, 53)
-                        .addComponent(jLabel3)))
-                .addContainerGap(280, Short.MAX_VALUE))
+                        .addComponent(jLabel4)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 262, Short.MAX_VALUE))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -73,22 +79,28 @@ public class analisadorPanel extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jLabel1)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
-                        .addGap(2, 2, 2)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 393, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(jLabel3))
-                .addContainerGap(12, Short.MAX_VALUE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                            .addComponent(jLabel4))
+                        .addGap(1, 1, 1)))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 393, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jScrollPane2))
+                .addContainerGap(103, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        // Obtém o texto digitado na jTextArea1
+        String textoDigitado = jTextArea1.getText();
+        
+        Lexico lexico = new Lexico();
+        String resultadoAnalise = lexico.analisa(textoDigitado);
+        
+        // Agora você pode fazer o que quiser com o textoDigitado, como exibi-lo em jTextPane1
+        jTextPane1.setText(resultadoAnalise);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
@@ -132,9 +144,10 @@ public class analisadorPanel extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JTextPane jTextPane1;
     // End of variables declaration//GEN-END:variables
 }
--- Analisador/src/Main/Main.java
+++ Analisador/src/Main/Main.java
@@ -1,17 +1,26 @@
 package Main;
 
 import Base.Lexico;
+import Grafico.analisadorPanel;
 
 public class Main {
     public static void main(String[] args) {
 //       String codigoFonte = "i if IF int x = 10 + 20; 01";
 //        String codigoFonte = "20;";
-        String codigoFonte = "2 eu sou a lenda IF if E else ";
+//        String codigoFonte = "2 eu sou a lenda IF if E else ";
+//
+//        Lexico lexico = new Lexico();
+//        String resultadoAnalise = lexico.analisa(codigoFonte);
+//
+//        System.out.println("Resultado da análise léxica:");
+//        System.out.println(resultadoAnalise);
+          // Criação e exibição da interface gráfica
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                analisadorPanel panel = new analisadorPanel();
+                panel.setVisible(true);
+            }
+        });
 
-        Lexico lexico = new Lexico();
-        String resultadoAnalise = lexico.analisa(codigoFonte);
-
-        System.out.println("Resultado da análise léxica:");
-        System.out.println(resultadoAnalise);
     }
 }
